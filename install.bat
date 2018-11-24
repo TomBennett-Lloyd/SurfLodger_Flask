6f835ffe180d22426a5030d4682ee3b86245cec1
TomBennett-Lloyd <36540971+TomBennett-Lloyd@users.noreply.github.com>
2018-11-24 18:29:18 +0000

    setup application

---
+++ install.bat
@@ -0,0 +1,5 @@
+python -m venv venv
+CALL SurfLodger\Scripts\activate
+pause
+pip install Flask
+SurfLodger
---
+++ startApp.bat
@@ -0,0 +1,5 @@
+CALL venv\Scripts\activate
+set FLASK_APP=flaskr
+set FLASK_ENV=development
+flask run
+pause
