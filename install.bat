c2f287f3c8b102032db15e65e0c2d0667409441d
TomBennett-Lloyd <36540971+TomBennett-Lloyd@users.noreply.github.com>
2018-11-27 05:03:43 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ python -m venv venv
 echo "activating the new virtual environment"
 CALL venv\Scripts\activate
 echo "installing the flask web application"
-pip install -e \
+pip install -e %cd%
 echo "launch application on localhost:5000"
 SurfLodger
